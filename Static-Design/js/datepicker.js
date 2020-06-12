4f24a9bf9723aba5de58b9eeb81d6f7de5c09779
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-06-11 21:53:01 -0700

    Finished the appointments page

--- Static-Design/js/datepicker.js
+++ Static-Design/js/datepicker.js
@@ -1,18 +1,5 @@
 
 
 $(document).ready(function () {
-    $("#datepicker").datepicker({
-        nextText: 'navigate_next',
-        prevText: 'navigate_before',
-    });
-
-    $(".ui-icon").addClass("material-icons");
-
-    $(".ui-datepicker-inline").on("click", function () {
-        $(".ui-icon").addClass("material-icons");
-    });
-
-    $("a").on("click", function () {
-        $(".ui-icon").addClass("material-icons");
-    });
+    $("#datepicker").datepicker();
 });
---
+++ Static-Design/js/time-slot.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".time-slot").click(function () {
+        $(".time-slot").css({ "background-color": "#fff", "color": "#292F36" });
+        $(this).css({ "background-color": "#08C188", "color": "#fff" });
+    });
+
+
+});
