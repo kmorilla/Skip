1129e43c318133994a5fb9983e29535a2681504f
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-06-18 21:36:32 -0700

    Added slide1 and slide2 on the index page

--- Static-Design/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var windowHeight = $(window).innerHeight();
-    console.log(windowHeight);
-
-    $(".slide-1").height(windowHeight);
-    $(".home2").height(windowHeight);
-    $(".home3").height(windowHeight);
-
-});
