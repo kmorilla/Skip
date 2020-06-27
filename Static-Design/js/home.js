db5252be7ab76614d3a7e6e26c86e83d4176f479
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-06-26 18:45:05 -0700

    Made the homepage dynamic
    Added the gas icon to fill up on touch.
    Added the Schedule Today wording fi there are no appointments

--- Static-Design/js/home.js
+++ Static-Design/js/home.js
@@ -11,12 +11,71 @@ $(document).ready(function () {
 
     $('.popup-text').click(function () {
         $(this).closest('.appointment').remove();
+        if ($('#appointments').find('div').hasClass('appointment')) {
+            $('.schedule-today').hide();
+        }
+        else {
+            $('.schedule-today').show();
+        }
     });
 
-    if (!$('#appointments').find('.appointment')) {
+    if ($('#appointments').find('div').hasClass('appointment')) {
+        $('.schedule-today').hide();
+    }
+    else {
         $('.schedule-today').show();
     }
 
-    console.log($('#appointments').find('.appointment'));
+    var click = 1;
+
+    $('.gas-icon').click(function () {
+        var transparentPercent;
+        var colorPercent;
+        var fillUps;
+
+        switch (click) {
+            case 1:
+                transparentPercent = 80;
+                colorPercent = 20;
+                fillUps = 4;
+                break;
+            case 2:
+                transparentPercent = 60;
+                colorPercent = 40;
+                fillUps = 3;
+                break;
+            case 3:
+                transparentPercent = 40;
+                colorPercent = 40;
+                fillUps = 2;
+                break;
+            case 4:
+                transparentPercent = 20;
+                colorPercent = 20;
+                fillUps = 1;
+                break;
+            case 5:
+                transparentPercent = 0;
+                colorPercent = 0;
+                fillUps = 0;
+                break;
+            default:
+                click = 0
+                transparentPercent = 100;
+                colorPercent = 0;
+                fillUps = 5;
+                break;
+        }
+
+        $('.gas-icon').css({ 'background': 'linear-gradient( transparent ' + transparentPercent + '% ,' + '#08C188' + ' ' + colorPercent + '%' });
+
+        $('#fill-ups').find('p>span').text(fillUps);
+
+        click++;
+    });
+
 
 });
+
+
+/* background: linear-gradient( transparent 60%, var(--accent-color) 40%); */
